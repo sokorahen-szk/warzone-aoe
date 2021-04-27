26c28b8d38b1cec619906a9698b0a9cc89cc25a1
sokorahen-szk <kamonotakahashi@gmail.com>
2021-04-27 13:22:11 +0900

    Registerユースケース

--- backend/app/Http/Controllers/AccountController.php
+++ backend/app/Http/Controllers/AccountController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Http\Requests\Account\AccountRegistrationRequest;
 use Package\Usecase\Account\GetInfo\AccountGetInfoCommand;
 use Package\Usecase\Account\GetInfo\AccountGetInfoServiceInterface;
 
 class AccountController extends Controller
 {
+    public function registration(AccountRegistrationRequest $request)
+    {
+        return null;
+    }
     public function show(AccountGetInfoServiceInterface $interactor)
     {
         $command = new AccountGetInfoCommand(\Auth::user()->id);
