49276b70336812a3157a4d7c2778cd4b2e6f0e84
sokorahen-szk <kamonotakahashi@gmail.com>
2021-05-04 23:17:02 +0900

    Middlewareの追加。heroku でhttps対応

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -77,6 +77,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (\App::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
