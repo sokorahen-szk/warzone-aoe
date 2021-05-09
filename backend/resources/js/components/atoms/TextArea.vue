e812edff66a21ba76dc116e80036ac815a36b403
sokorahen-szk <kamonotakahashi@gmail.com>
2021-05-09 17:20:16 +0900

    新規登録リクエストページ

---
+++ backend/resources/js/components/atoms/TextArea.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-textarea
+    :solo="solo"
+    :outlined="outlined"
+    :name="name"
+    :value="value"
+    :label="label"
+    :placeholder="placeholder"
+  ></v-textarea>
+</template>
+
+<style scoped>
+* >>> .v-messages {
+  min-height: 0px !important;
+}
+</style>
+
+<script>
+export default {
+  name: 'TextArea',
+  props: {
+    label: { type: String, default: '' },
+    placeholder: { type: String, default: '' },
+    value: { type: String, default: '' },
+    name: { type: String, default: 'name' },
+    solo: { type: Boolean, default: false },
+    outlined: { type: Boolean, default: false },
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
--- backend/resources/js/components/pages/admin/Request.vue
+++ backend/resources/js/components/pages/admin/Request.vue
@@ -6,7 +6,48 @@
 			</template>
 			<template slot="container">
 				<v-container>
-          あ
+          <div class="subtitle-2">
+            <v-row no-gutters no-wrap class="text-right">
+              <v-col cols="12">新規リクエスト数：{{requests.length}} 件</v-col>
+              <v-col cols="12">承認数：0 件</v-col>
+              <v-col cols="12">キャンセル数：0 件</v-col>
+            </v-row>
+          </div>
+          <v-list class="pa-0 ma-0">
+            <template v-for="request in requests">
+            <v-list-item class="pa-0 ma-0" :key="`v-list-item-${request.id}`">
+              <v-list-item-content>
+                <v-list-item-title>{{request.player_name}}</v-list-item-title>
+                <v-list-item-subtitle>参加日：{{request.joinedAt}}</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-row no-gutters>
+                  <v-col>
+                    <v-icon @click="edit(request.id)">mdi-clipboard-edit-outline</v-icon>
+                  </v-col>
+                  <v-col>
+                    <v-icon color="green">mdi-check-bold</v-icon>
+                  </v-col>
+                  <v-col>
+                    <v-icon color="red">mdi-close-thick</v-icon>
+                  </v-col>
+                </v-row>
+              </v-list-item-action>
+            </v-list-item>
+            <v-card :key="`v-card-${request.id}`" class="pa-4">
+              <TextArea
+                outlined
+                placeholder="備考"
+                class="pa-0 ma-0"
+              />
+              <div class="text-right">
+                <Button color="success" label="承認" />
+                <Button color="error" label="拒否" />
+              </div>
+            </v-card>
+            <v-divider :key="`v-list-item-divider-${request.id}`" />
+            </template>
+          </v-list>
         </v-container>
 			</template>
     </CommonWithRightColumnTemplate>
@@ -16,15 +57,27 @@
 <script>
 import CommonWithRightColumnTemplate from '@/components/templates/CommonWithRightColumnTemplate'
 import AccountRightMenu from '@/components/organisms/AccountRightMenu'
+import TextArea from '@/components/atoms/TextArea'
+import Button from '@/components/atoms/Button'
 export default {
   name: 'Request',
   components: {
     CommonWithRightColumnTemplate,
-    AccountRightMenu
+    AccountRightMenu,
+    TextArea,
+    Button
   },
   data() {
     return {
-      //
+      requests: [
+        {id: 2, player_name: "テストユーザ", joinedAt: "2021-03-22 00:00:00"},
+        {id: 3, player_name: "テストユーザa", joinedAt: "2021-04-19 12:00:00"},
+      ]
+    }
+  },
+  methods: {
+    edit(id) {
+      console.log(id)
     }
   }
 }
