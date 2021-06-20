caaf577d8ce254495d56fccbdc03d75c462f57fc
sokorahen-szk <kamonotakahashi@gmail.com>
2021-06-20 18:42:00 +0900

    ogp設定

--- backend/config/web.php
+++ backend/config/web.php
@@ -1,6 +1,21 @@
 <?php
-
+$siteName = "warzone-aoe";
+$description = "SteamゲームのAoE2DE/AoE2HDの日本最大級コミュニティサイト！Warzone独自レートでマルチゲームを体験しよう！初心者歓迎！AI・チーム戦・マッチ戦";
 return [
     // サイト名
-    "web_site_name" => "warzone-aoe",
+    "web_site_name"             => $siteName,
+
+    // サイト説明
+    "description"               => $description,
+
+    // サイトキーワード
+    "keywords"                  => "AgeOfEmpires2,AoE2,AoE2DE,AoE2HD,warzone,warzone-aoe",
+
+    "og" => [
+        "locale"                => "ja_JP",
+        "title"                 => $siteName,
+        "type"                  => "website",
+        "image"                 => "/warzone-aoe.png",
+        "description"           => $description,
+    ],
 ];
--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -3,15 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ \Config::get('web.web_site_name') }}</title>
-    <meta name="description" itemprop="description" content="test">
-    <meta name="keywords" itemprop="keywords" content="test">
+    <meta name="description" itemprop="description" content="{{ \Config::get('web.description') }}">
+    <meta name="keywords" itemprop="keywords" content="{{ \Config::get('web.keywords') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:locale" content="{{ \Config::get('web.og.locale') }}">
+    <meta property="og:title" content="{{ \Config::get('web.og.title') }}">
+    <meta property="og:type" content="{{ \Config::get('web.og.type') }}">
+    <meta property="og:site_name" content="{{ \Config::get('web.og.title') }}" />
+    <meta property="og:image" content="{{ url(\Config::get('web.og.image')) }}">
+    <meta property="og:description" content="{{ \Config::get('web.og.description') }}">
+    <link rel="stylesheet" href="{{ url(mix('/css/app.css')) }}">
 </head>
 <body>
     <div id="app"></div>
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <script src="{{ url(mix('/js/app.js')) }}" defer></script>
 </body>
 </html>
