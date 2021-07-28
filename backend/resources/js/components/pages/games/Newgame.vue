c7a99a8992aef8e52684ed3da0c32a98e8c11083
sokorahen-szk <kamonotakahashi@gmail.com>
2021-07-28 23:03:13 +0900

    ゲーム作成画面のUI変更

--- backend/resources/js/components/pages/games/Newgame.vue
+++ backend/resources/js/components/pages/games/Newgame.vue
@@ -7,14 +7,14 @@
 
     <v-row no-gutters>
       <v-col cols="12" sm="12" md="5" lg="4" class="px-2" :style="addStyle">
-        <div class="py-2">プレイヤー選択</div>
+        <div class="py-2 text-h6">プレイヤー選択</div>
         <PlayerSearchBox
           :keyword="search"
           :players="players"
           @update="updatePlayer"
         />
 
-        <div class="py-2">ゲーム設定</div>
+        <div class="py-2 text-h6">ゲーム設定</div>
         <v-row no-gutters>
           <v-col cols="12" class="mb-2">
             <Select
@@ -22,20 +22,24 @@
               :items="gamePackages"
               :selectedIndex="selectedGamePackageId"
               @input="selectedGamePackageId = $event"
+              :disabled="gamePackages.length < 1"
             />
           </v-col>
           <v-col cols="12" class="mb-2">
             <Select
               label="ルールを選択する"
-              :items="gameMaps"
-              :selectedIndex="selectedMapId"
-              @input="selectedMapId = $event"
+              :items="gameRules"
+              :selectedIndex="selectedRuleId"
+              @input="selectedRuleId = $event"
             />
           </v-col>
           <v-col cols="12">
             <Select
               label="マップを選択する"
-              :items="items"
+              :items="gameMaps"
+              :selectedIndex="selectedMapId"
+              @input="selectedMapId = $event"
+              :disabled="gameMaps.length < 1"
             />
           </v-col>
         </v-row>
@@ -44,47 +48,62 @@
       <v-col cols="12" sm="12" md="7" lg="8">
 
         <v-row no-gutters>
-          <v-col cols="12" style="min-height: 450px;" class="pa-2">
-            <div class="text-center mb-2">
-              選択プレイヤー ({{selectedPlayers.length}}/8) 人
-            </div>
-            <v-card v-for="player in selectedPlayers" :key="player.id" class="headline py-4 mb-2">
-              <v-row no-gutters>
-                <v-col class="pl-2">{{player.name}}</v-col>
-                <v-col>rank:{{player.rank}}</v-col>
-                <v-spacer />
-                <v-col cols="1" class="text-center">
-                  <v-icon color="red" @click="deletePlayer(player.id)">mdi-delete</v-icon>
-                </v-col>
+          <v-col cols="12" style="min-height: 500px;" class="pa-2">
+            <v-card height="500" class="pa-2">
+              <div class="text-center mb-2 text-h6">
+                選択プレイヤー ({{selectedPlayers.length}}/8) 人
+              </div>
+              <v-card
+                v-for="player in selectedPlayers"
+                :key="player.id"
+                class="caption py-2 mb-2"
+                outlined
+              >
+                <v-row no-gutters>
+                  <v-col class="pl-2">{{player.name}}</v-col>
+                  <v-col>rank:{{player.rank}}</v-col>
+                  <v-spacer />
+                  <v-col cols="1" class="text-center">
+                    <v-icon
+                      left
+                      size="18"
+                      color="red"
+                      @click="deletePlayer(player.id)"
+                    >
+                      mdi-delete
+                    </v-icon>
+                  </v-col>
+                </v-row>
+              </v-card>
+              <v-card
+                v-for="n of (8 - selectedPlayers.length)"
+                :key="`player-${ selectedPlayers.length + n}`"
+                class="caption pa-2 mb-2"
+                color="#FAFAFA"
+                :elevation="0"
+              >
+                プレイヤーを選択
+              </v-card>
+
+              <v-row no-gutters justify="center" align-content="center">
+                <Button
+                  class="mr-2"
+                  color="info"
+                  label="チーム分け"
+                  width="200"
+                  height="55"
+                  :disabled="selectedPlayers.length < 2"
+                />
+                <Button
+                  class="ml-2"
+                  color="warning"
+                  label="クリア"
+                  width="200"
+                  height="55"
+                  @click="clearEvent"
+                />
               </v-row>
             </v-card>
-          </v-col>
-          <v-col cols="12">
-            <v-row no-gutters justify="center" align-content="center">
-
-              <v-row no-gutters>
-                <v-col cols="6" sm="6" md="6" lg="6" xl="6">
-                  <Button
-                    class="mr-2"
-                    color="info"
-                    label="チーム分け"
-                    height="55"
-                    block
-                  />
-                </v-col>
-                <v-col cols="6" sm="6" md="6" lg="6" xl="6">
-                  <Button
-                    class="ml-2"
-                    color="warning"
-                    label="クリア"
-                    height="55"
-                    @click="clearEvent"
-                    block
-                  />
-                </v-col>
-              </v-row>
-
-            </v-row>
           </v-col>
         </v-row>
 
@@ -187,10 +206,13 @@ export default {
       selectedPlayers: [],
       selectedGamePackageId: 0,
       selectedMapId: 0,
-      gamePackages: [gamePackageTemplate],
-      gameMaps: [gameMapTemplate],
-      items: [
-        {id: 1, value: "A", label: "AAA"}
+      selectedRuleId: 0,
+      gamePackages: [],
+      gameMaps: [],
+
+      // TODO: ルールのAPIがまだのため、ダミーデータを渡しておく
+      gameRules: [
+        {id: 1, value: "サンプル", label: "サンプル"}
       ]
     }
   }
